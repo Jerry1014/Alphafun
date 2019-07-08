a72987496374b07242a225d5749cc9400887ea4d
Jerry <757320383@qq.com>
2019-07-08 14:16:46 +0800

    完成迭代求极大极小值部分，等待完成评估函数

--- AlphaFun.py
+++ AlphaFun.py
@@ -8,27 +8,67 @@ GRID_WIDTH = 40
 SIZE_OF_CHESSMAN = 16
 COLUMN = 15
 ROW = 15
-
-# 棋型的评估分数
-shape_score = {
-    ()
-}
+DEPTH = 1  # 搜索深度，每一"层"包括两层（极大极小）
 
 white_situation = np.array([[0 for i in range(COLUMN)] for j in range(ROW)])
 black_situation = np.array([[0 for i in range(COLUMN)] for j in range(ROW)])
 all_situation = np.array([[0 for i in range(COLUMN)] for j in range(ROW)])
+
+# 限定搜索范围，加快搜索速度
+range_x_min = COLUMN // 2
+range_y_min = ROW // 2
+range_x_max = COLUMN // 2
+range_y_max = ROW // 2
 
 
 def ai():
     """使用极大极小指下棋
     :return:tuple ai选择的下棋位置
     """
-    return random.randint(0, ROW-1), random.randint(0, COLUMN-1)
+    return each_it(1)[1]
 
 
-def chessboard_evaluation():
+def each_it(deep):
+    max_point = -sys.maxsize
+    pos = (0, 0)
+    for j in range(range_x_min, range_x_max):
+        for i in range(range_y_min, range_y_max):
+            if all_situation[i][j] == 0:
+                all_situation[i][j] = -1
+                # ai试探下一步
+
+                min_point = sys.maxsize
+                break_sign = False
+                for j in range(range_x_min, range_x_max):
+                    for i in range(range_y_min, range_y_max):
+                        # 模拟用户下一步
+                        if all_situation[i][j] == 0:
+                            all_situation[i][j] = 1
+                            # 若探索深度未到，则递归
+                            if deep < DEPTH:
+                                tem = each_it(deep + 1)
+                            else:
+                                tem = get_score()
+                            if tem < max_point:
+                                # 减枝
+                                break_sign = True
+                                break
+                            elif tem < min_point:
+                                min_point = tem
+                            all_situation[i][j] = 0
+
+                    if break_sign:
+                        break
+                if not break_sign:
+                    max_point = min_point
+                    pos = (i, j)
+                all_situation[i][j] = 0
+    return max_point, pos
+
+
+def get_score():
     """对当前局势进行评估，并给出分数"""
-    pass
+    return 1
 
 
 def check_win(check_situation):
@@ -66,57 +106,74 @@ def check_win(check_situation):
     return False
 
 
-# 棋盘初始化
-win = GraphWin("Battle with AlphaFun", GRID_WIDTH * (COLUMN - 1), GRID_WIDTH * (ROW - 1))
-win.setBackground("yellow")
+def main():
+    # 棋盘初始化
+    global range_x_min, range_x_max, range_y_min, range_y_max
+    win = GraphWin("Battle with AlphaFun", GRID_WIDTH * (COLUMN - 1), GRID_WIDTH * (ROW - 1))
+    win.setBackground("yellow")
 
-i1 = 0
-while i1 < GRID_WIDTH * (COLUMN - 1):
-    Line(Point(i1, 0), Point(i1, GRID_WIDTH * COLUMN)).draw(win)
-    i1 = i1 + GRID_WIDTH
-i2 = 0
-while i2 < GRID_WIDTH * (ROW - 1):
-    Line(Point(0, i2), Point(GRID_WIDTH * ROW, i2)).draw(win)
-    i2 = i2 + GRID_WIDTH
+    i1 = 0
+    while i1 < GRID_WIDTH * (COLUMN - 1):
+        Line(Point(i1, 0), Point(i1, GRID_WIDTH * COLUMN)).draw(win)
+        i1 = i1 + GRID_WIDTH
+    i2 = 0
+    while i2 < GRID_WIDTH * (ROW - 1):
+        Line(Point(0, i2), Point(GRID_WIDTH * ROW, i2)).draw(win)
+        i2 = i2 + GRID_WIDTH
 
-while True:
-    # todo 默认为使用者先手，未来改为可选择
-    # todo 未对下棋胜负进行分析
-    # 玩家下棋
-    player_mouse_pos = win.getMouse()
-    player_pos = (round((player_mouse_pos.getY()) / GRID_WIDTH), round((player_mouse_pos.getX()) / GRID_WIDTH))
-    while all_situation[player_pos[0]][player_pos[1]] != 0:
+    while True:
+        # todo 默认为使用者先手，未来改为可选择
+        # 玩家选择下棋位置
         player_mouse_pos = win.getMouse()
-        player_pos = (round((player_mouse_pos.getY()) / GRID_WIDTH, round((player_mouse_pos.getX()) / GRID_WIDTH)))
-    all_situation[player_pos[0]][player_pos[1]] = 1
-    black_situation[player_pos[0]][player_pos[1]] = 1
-    piece = Circle(Point(GRID_WIDTH * player_pos[1], GRID_WIDTH * player_pos[0]), SIZE_OF_CHESSMAN)
-    piece.setFill('black')
-    piece.draw(win)
+        player_pos = (round((player_mouse_pos.getY()) / GRID_WIDTH), round((player_mouse_pos.getX()) / GRID_WIDTH))
+        while all_situation[player_pos[0]][player_pos[1]] != 0:
+            player_mouse_pos = win.getMouse()
+            player_pos = (round((player_mouse_pos.getY()) / GRID_WIDTH, round((player_mouse_pos.getX()) / GRID_WIDTH)))
+        all_situation[player_pos[0]][player_pos[1]] = 1
+        black_situation[player_pos[0]][player_pos[1]] = 1
+        piece = Circle(Point(GRID_WIDTH * player_pos[1], GRID_WIDTH * player_pos[0]), SIZE_OF_CHESSMAN)
+        piece.setFill('black')
+        piece.draw(win)
 
-    if check_win(black_situation):
-        Text(Point(100, 120), "黑棋胜利").draw(win)
-        print('黑棋胜利')
-        win.getMouse()
-        break
+        # 检查游戏是否结束
+        if check_win(black_situation):
+            Text(Point(100, 120), "黑棋胜利").draw(win)
+            print('黑棋胜利')
+            win.getMouse()
+            break
 
-    # AI下棋
-    ai_pos = ai()
-    if all_situation[ai_pos[0]][ai_pos[1]] != 0:
-        Text(Point(100, 120), "AI投降").draw(win)
-        print('AI投降')
-        win.getMouse()
-        break
-    all_situation[ai_pos[0]][ai_pos[1]] = -1
-    white_situation[ai_pos[0]][ai_pos[1]] = 1
-    piece = Circle(Point(GRID_WIDTH * ai_pos[0], GRID_WIDTH * ai_pos[1]), SIZE_OF_CHESSMAN)
-    piece.setFill('white')
-    piece.draw(win)
+        # 修改搜索范围
+        if player_pos[0] - 2 < range_x_min:
+            range_x_min = player_pos[0] - 2 if player_pos[0] - 2 > 0 else 0
+        if player_pos[0] + 2 > range_x_max:
+            range_x_max = player_pos[0] + 2 if player_pos[0] + 2 > COLUMN else COLUMN + 1
+        if player_pos[1] - 2 < range_y_min:
+            range_y_min = player_pos[1] - 2 if player_pos[1] - 2 > 0 else 0
+        if player_pos[1] + 2 > range_y_max:
+            range_y_max = player_pos[1] + 2 if player_pos[1] + 2 > ROW else ROW + 1
 
-    if check_win(black_situation):
-        Text(Point(100, 120), "白棋胜利").draw(win)
-        print('白棋胜利')
-        win.getMouse()
-        break
+        # AI下棋
+        ai_pos = ai()
+        if all_situation[ai_pos[0]][ai_pos[1]] != 0:
+            Text(Point(100, 120), "AI投降").draw(win)
+            print('AI投降')
+            win.getMouse()
+            break
+        all_situation[ai_pos[0]][ai_pos[1]] = -1
+        white_situation[ai_pos[0]][ai_pos[1]] = 1
+        piece = Circle(Point(GRID_WIDTH * ai_pos[0], GRID_WIDTH * ai_pos[1]), SIZE_OF_CHESSMAN)
+        piece.setFill('white')
+        piece.draw(win)
 
-win.close()
+        # 检查游戏是否结束
+        if check_win(black_situation):
+            Text(Point(100, 120), "白棋胜利").draw(win)
+            print('白棋胜利')
+            win.getMouse()
+            break
+
+    win.close()
+
+
+if __name__ == '__main__':
+    main()
